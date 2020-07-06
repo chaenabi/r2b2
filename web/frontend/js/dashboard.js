07bf63b891633504a01954a3531e59ae9b9de42f
chaenabi <find1086@gmail.com>
2020-07-06 23:20:52 +0900

    add r2d2 door status toggle

---
+++ web/frontend/js/dashboard.js
@@ -0,0 +1,21 @@
+// var first = new mojs.Shape({
+//     shape: 'circle',
+//     radius: {
+//       0: 40
+//     },
+//     stroke: 'gold',
+//     strokeWidth: {
+//       20: 0
+//     },
+//     fill: 'none',
+//     left: 0,
+//     top: 0,
+//     duration:300
+//   });
+
+//   document.addEventListener('click', function (e) {
+//     first.tune({
+//       x: e.pageX,
+//       y: e.pageY
+//     }).replay();
+//   })
