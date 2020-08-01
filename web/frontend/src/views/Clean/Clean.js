ce1095b192a7bb262db0c40d25d0104db7c564c7
chaenabi <find1086@gmail.com>
2020-08-02 03:42:37 +0900

    update clean.js ratency

--- web/frontend/src/views/Clean/Clean.js
+++ web/frontend/src/views/Clean/Clean.js
@@ -30,43 +30,63 @@ function UserProfile() {
   // }
   
   const tableName = "온도 습도 미세먼지 데이터 수신테스트";
-  const [dataList, setDataList] = useState([]);
+  const [dataList, setDataList] = useState([0]);
+  const [result, setResult] = useState([]);
   const [temperature, setTemperature] = useState(0);
   const [humid, setHumid] = useState(0);
   const [pm1, setPm1] = useState(0);
   const [pm10, setPm10] = useState(0);
   const [pm25, setPm25] = useState(0);
-  const [insertDate, setInsertDate] = useState('');
+  const [date, setDate] = useState('');
+  const [idx, setIdx] = useState(0);
 
-    const getData = 
-      useEffect(() => { 
-      setTimeout(() => {
-         axios.get('http://211.229.91.230:3000/api/r2d2')
-              .then(res => {
-                let parseData = JSON.parse(JSON.stringify(res.data[0]));  
-                setTemperature(parseData.r2d2_temperature);
-                setHumid(parseData.r2d2_humidity);
-                setPm1(parseData.r2d2_particulate_matter1);
-                setPm10(parseData.r2d2_particulate_matter2);
-                setPm25(parseData.r2d2_particulate_matter3);
-                setInsertDate(parseData.insert_date);               
-                setDataList(prev => [
-                  ...prev,
-                  [temperature, humid, pm1, pm10, pm25, insertDate],
-                ])
+  useEffect(() => {
+    setTimeout(() => {
+          axios.get('http://211.229.91.230:3000/api/r2d2')
+               .then((res) => {
+                 let result = JSON.parse(JSON.stringify(res));
+                 setDataList(result.data);
               })
-    }, 4000);
-  },[dataList]);
+              .catch(err => console.log(err));
+    }, 6000)
+}, [dataList]);
+
+useEffect(() => {     
+  setTimeout(() => {
+    setIdx(idx+1);
+  }, 1100);
+
+    setTimeout(() => {
+          setTemperature(dataList[idx].r2d2_temperature);
+          setHumid(dataList[idx].r2d2_humidity);
+          setPm1(dataList[idx].r2d2_particulate_matter1);
+          setPm25(dataList[idx].r2d2_particulate_matter2);
+          setPm10(dataList[idx].r2d2_particulate_matter3);
+          setDate(dataList[idx].insert_date);
+        setResult(prev => [
+          ...prev,
+          [temperature,
+          humid,
+          pm1,
+          pm25,
+          pm10,
+          date
+          ]
+          
+        ])
+    }, 5000) 
+  
+}, [dataList]);
+
   //render(){
     return (
-      {getData},
       <div className="content-wrap">
         <div className="clean-table-wrap">
           <h2><FontAwesomeIcon icon={faList} />{tableName}</h2>
         <Table
              tableHeaderColor="primary"
              tableHead={["온도", "습도", "초미세먼지(pm1.0)", "초미세먼지(pm2.5)", "미세먼지(pm10)", "수신날짜"]}
-             tableData={dataList}
+             tableData={result}
            />
         </div>
       </div>
